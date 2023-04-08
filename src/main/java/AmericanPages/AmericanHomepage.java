25f13277450fe32f65fe0b82eb7723d49cce1697
Amiazi <aniqamiazi424@gmail.com>
2023-04-08 18:34:10 -0400

    fixed project structure

---
+++ src/main/java/AmericanPages/AmericanHomepage.java
@@ -0,0 +1,25 @@
+package AmericanPages;
+
+import common.WebAPI;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class AmericanHomepage extends WebAPI {
+    //driver.findElement(By.xpath)
+
+@FindBy(partialLinkText = "We're hiring! Join o")
+public WebElement careerLink;
+    public AmericanHomepage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
+    //method region
+
+    public void clickOnCareerLink(){
+        careerLink.click();
+    }
+
+}
